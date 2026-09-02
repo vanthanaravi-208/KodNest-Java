0a03126e8b26b00f4261e0796ca47e01b7a000e2
vanthanaravi-208 <vanthanaravi.r@gmail.com>
2026-09-02 10:59:52 +0530

    this is my first update

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,6 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
-       //Write your code here
+       System.out.println("Welcome to java");
+       System.out.println("I am in Java Track");
     }
 }
