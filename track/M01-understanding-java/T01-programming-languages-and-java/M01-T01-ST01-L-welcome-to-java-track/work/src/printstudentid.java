0e6473ee868029d3a43e3f8c9f51e53f6780a7f4
vanthanaravi-208 <vanthanaravi.r@gmail.com>
2026-09-05 11:54:42 +0530

    this is third code

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/printstudentid.java
@@ -0,0 +1,10 @@
+public class printstudentid {
+    public static void main(String[] args) {
+        System.out.println("--------------------");
+        System.out.println("STUDENT PROFILE");
+        System.out.println("Name: Alex");
+        System.out.println("Course: Beginner");
+        System.out.println("Level: Beginner");
+        System.out.println("--------------------");
+    }
+}
