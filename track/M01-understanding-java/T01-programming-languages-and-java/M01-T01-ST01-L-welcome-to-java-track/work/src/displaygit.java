8a9a62782ccc8059dcfc14b50ff984b9ab7e320e
vanthanaravi-208 <vanthanaravi.r@gmail.com>
2026-09-05 11:44:15 +0530

    this is second code

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/displaygit.java
@@ -0,0 +1,9 @@
+class displaygit {
+    public static void main(String[] args) {
+        // Display the repository cloning status
+        System.out.println("Repository: Cloned Successfully");
+
+        // Display whether the project is ready
+        System.out.println("Project Status: Ready for Development");
+    }
+}
